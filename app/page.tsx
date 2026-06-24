8f1b6460a3e3146709098ff51885c6b0bbe387dc
ODEF <103530614+ODEF@users.noreply.github.com>
2026-06-25 02:46:15 +0400

    v1_1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ export default function HomePage() {
   return (
     <main>
       <h1>Healthy Paw</h1>
-      <p>Website is working on Vercel.</p>
+      <p>Webbsite is working on Vercel.</p>
     </main>
   );
 }
+
