3f89cfecb43dc9251e3ae5fd7ffd6033750123ee
ODEF <103530614+ODEF@users.noreply.github.com>
2026-06-25 02:53:47 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,30 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import React from 'react';
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+export default function HomePage() {
   return (
-    import type { Metadata } from "next";
-import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Healthy Paw",
-  description: "Wellness website",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body><h1>Healthy Paw</h1><p>Webbsite is working on Vercel.</p></body>
-    </html>
-    )
-  }
+    <main>
+      <h1>Healthy Paw</h1>
+      <p>Webbsite is working on Vercel.</p>
+    </main>
+  );
 }
+
