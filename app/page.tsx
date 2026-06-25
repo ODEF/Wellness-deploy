381479c08711b7cc6abf81f508274589c0a73dfe
ODEF <103530614+ODEF@users.noreply.github.com>
2026-06-25 21:06:25 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,8 @@
-import { createClient } from "../lib/supabase/server";
-
-export default async function HomePage() {
-  const supabase = await createClient();
-
-  const { data: todos, error } = await supabase.from("todos").select("*");
-
-  if (error) {
-    return (
-      <main style={{ padding: "40px" }}>
-        <h1>Supabase error</h1>
-        <pre>{error.message}</pre>
-      </main>
-    );
-  }
-
+export default function HomePage() {
   return (
     <main style={{ padding: "40px" }}>
       <h1>Healthy Paw</h1>
-
-      <ul>
-        {todos?.map((todo) => (
-          <li key={todo.id}>{todo.name}</li>
-        ))}
-      </ul>
+      <p>Website is running.</p>
     </main>
   );
 }
-// import { createClient } from '@/utils/supabase/server'
-// import { cookies } from 'next/headers'
-
-// export default async function Page() {
-//   const cookieStore = await cookies()
-//   const supabase = createClient(cookieStore)
-
-//   const { data: todos } = await supabase.from('todos').select()
-
-//   return (
-    // <ul>
-    //   {todos?.map((todo) => (
-    //     <li key={todo.id}>{todo.name}</li>
-    //   ))}
-    // </ul>
-//   )
-// }
